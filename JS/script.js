74e6f5cfe33563e2cb3f6efc5f7f83543b92a0ea
Akande Kehinde <kehindeakande067@gmail.com>
2025-03-26 13:52:28 +0100

    add duplication of the first function to get other functions

--- JS/script.js
+++ JS/script.js
@@ -6,49 +6,49 @@ $(document).ready(function () {
         $('.view1').toggleClass('is-flipped')
     })
 
-    // $('.view2').click(function () {
-    //     $('.view2').toggleClass('is-flipped')
-    //  })
-    // $('.view3').click(function () {
-    //     $('.view3').toggleClass('is-flipped')
+    $('.view2').click(function () {
+        $('.view2').toggleClass('is-flipped')
+     })
+    $('.view3').click(function () {
+        $('.view3').toggleClass('is-flipped')
        
-    // })
-    // $('.view4').click(function () {
-    //     $('.view4').toggleClass('is-flipped')
+    })
+    $('.view4').click(function () {
+        $('.view4').toggleClass('is-flipped')
        
-    // })
-    // $('.view5').click(function () {
-    //     $('.view5').toggleClass('is-flipped')
+    })
+    $('.view5').click(function () {
+        $('.view5').toggleClass('is-flipped')
        
-    // })
-    // $('.view6').click(function () {
-    //     $('.view6').toggleClass('is-flipped')
+    })
+    $('.view6').click(function () {
+        $('.view6').toggleClass('is-flipped')
        
-    // })
-    // $('.view7').click(function () {
-    //     $('.view7').toggleClass('is-flipped')
+    })
+    $('.view7').click(function () {
+        $('.view7').toggleClass('is-flipped')
        
-    // })
-    // $('.view8').click(function () {
-    //     $('.view8').toggleClass('is-flipped')
+    })
+    $('.view8').click(function () {
+        $('.view8').toggleClass('is-flipped')
        
-    // })
-    // $('.view9').click(function () {
-    //     $('.view9').toggleClass('is-flipped')
+    })
+    $('.view9').click(function () {
+        $('.view9').toggleClass('is-flipped')
 
-    // })
-    // $('.view10').click(function () {
-    //     $('.view10').toggleClass('is-flipped')
+    })
+    $('.view10').click(function () {
+        $('.view10').toggleClass('is-flipped')
        
-    // })
-    // $('.view11').click(function () {
-    //     $('.view11').toggleClass('is-flipped')
+    })
+    $('.view11').click(function () {
+        $('.view11').toggleClass('is-flipped')
        
-    // })
-    // $('.view12').click(function () {
-    //     $('.view12').toggleClass('is-flipped')
+    })
+    $('.view12').click(function () {
+        $('.view12').toggleClass('is-flipped')
        
-    // })
+    })
 
 
 
